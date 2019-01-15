d3ed29b2758272c44e1f0ca3c7ddb0c052d9f84c
JohanGro <JohanGro2004@gmail.com>
2019-01-15 08:42:36 -0800

    hangman progress

--- Hangman - Johan Gonzalez.py
+++ Hangman - Johan Gonzalez.py
@@ -2,4 +2,29 @@ import random
 word_bank = ["dog"]
 random_word = random.choice(word_bank)
 word = list(random_word)
-print(word)
+answer = "".join(word)
+stars = ""
+for character in word:
+    stars = stars + "-"
+    answer = answer.replace(answer, stars)
+print(answer)
+correct = 5
+while correct > 0:
+    guess = input("insert guess")
+    answer = list(answer)
+    if answer is word:
+        print("congratulations!")
+        exit(0)
+    if guess in answer:
+        print("you already guessed this")
+    if guess in word:
+        current_index = word.index(guess)
+        answer.pop(current_index)
+        answer.insert(current_index, guess)
+        answer = "".join(answer)
+        print(answer)
+    else:
+        answer = "".join(answer)
+        print("nope")
+        correct = correct - 1
+        print(answer)

f203f6968ade429c8ebbe519b2ef952e1fd118ff
JohanGro <JohanGro2004@gmail.com>
2019-01-23 08:43:16 -0800

    hangman completion?

--- Hangman - Johan Gonzalez.py
+++ Hangman - Johan Gonzalez.py
@@ -1,39 +1,52 @@
 import random
-word_bank = ["treble clef"]
+word_bank = ["treble clef", "music theory", "cow", "dog", "animal", "computer", "mouse", "crackers",
+             "grappling hook", "stringed instruments", "drought", "red", "drag", "dripping",
+             "scary", "ground", "electricity", "internet", "living room", "blue", "cyan", "tyrannosaurus rex"
+             "ripping", "pig", "chicken", "frog", "water", "wandering", "training", "reading is good",
+             "how are you doing?" "hows the weather?", "playing games", "reading books", "squashing the bugs",
+             "cleaning the code"]
+guessed = []
 random_word = random.choice(word_bank)
 word = list(random_word)
+true = list(word)
 answer = "".join(word)
 line = ""
 space = " "
+question_mark = "?"
 for character in word:
+    if character is question_mark:
+        line = line + "?"
     if character is space:
         line = line + " "
     else:
         line = line + "-"
         answer = answer.replace(answer, line)
-print(answer)
 correct = 5
 win = False
 while correct > 0 and win is False:
+    print(answer)
     answer = list(answer)
-    if word == answer:
+    if true == answer:
         win = True
         answer = "".join(answer)
         print("congratulations! the word was %s" % answer)
         exit()
+    print("letters you've guessed - %s " % guessed)
     guess = input("insert guess")
+    guessed.append(guess)
     guess = guess.lower()
     if guess in answer:
         print("you already guessed this")
-
-    if guess in word and guess not in answer:
+    while guess in word:
+        answer = list(answer)
         current_index = word.index(guess)
+        word.pop(current_index)
+        word.insert(current_index, "-")
         answer.pop(current_index)
         answer.insert(current_index, guess)
         answer = "".join(answer)
-        print(answer)
     if guess not in word and guess not in answer:
         answer = "".join(answer)
         print("nope")
         correct = correct - 1
-        print(answer)
+print("you could'nt guess it :/")
--- practice.py
+++ practice.py
@@ -1,13 +1,23 @@
 word = ["animals"]
+answer = "".join(word[0])
 list1 = "".join(word[0])
-list1 = list(list1)
 guesses_left = 2
+line = ""
+space = " "
+for character in list1:
+    if character is space:
+        line = line + " "
+    else:
+        line = line + "-"
+        list1 = list1.replace(list1, line)
+print(list1)
 while guesses_left > 0:
+    ans
+    list1 = list(list1)
     guess = input("insert a guess")
     for character in list1:
         if character == guess:
-            # replace with a *
-            current_index = list1.index(character)
+            current_index = answer.index(character)
             list1.pop(current_index)
-            list1.insert(current_index, "*")
+            list1.insert(current_index, guess)
     print(list1)

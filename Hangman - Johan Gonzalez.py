0e46de4f17f56071126ed2fb776680860fcd8437
JohanGro <JohanGro2004@gmail.com>
2019-01-24 08:44:09 -0800

    more hangman

--- Hangman - Johan Gonzalez.py
+++ Hangman - Johan Gonzalez.py
@@ -3,8 +3,11 @@ word_bank = ["treble clef", "music theory", "cow", "dog", "animal", "computer", 
              "grappling hook", "stringed instruments", "drought", "red", "drag", "dripping",
              "scary", "ground", "electricity", "internet", "living room", "blue", "cyan", "tyrannosaurus rex"
              "ripping", "pig", "chicken", "frog", "water", "wandering", "training", "reading is good",
-             "how are you doing?" "hows the weather?", "playing games", "reading books", "squashing the bugs",
-             "cleaning the code"]
+             "how are you doing?", "hows the weather?", "playing games", "reading books", "squashing the bugs",
+             "cleaning the code", "potato", "tomato", "turkey", "potted plant", "corn", "town", "mayor",
+             "painting", "switch", "cpu", "internet browser", "starry sky", "flag", "train", "cruel", "tree",
+             "earthy", "serious", "answer", "kick", "oceanic", "beautiful", "tiger", "history", "plasma",
+             "gateway", "forty", "llama", "cactus", "camel", "chocolate", "harmony", "world", "coral reef"]
 guessed = []
 random_word = random.choice(word_bank)
 word = list(random_word)
@@ -21,7 +24,7 @@ for character in word:
     else:
         line = line + "-"
         answer = answer.replace(answer, line)
-correct = 5
+correct = 7
 win = False
 while correct > 0 and win is False:
     print(answer)
@@ -31,12 +34,15 @@ while correct > 0 and win is False:
         answer = "".join(answer)
         print("congratulations! the word was %s" % answer)
         exit()
-    print("letters you've guessed - %s " % guessed)
+    guessed = "".join(guessed)
+    print("letters you've guessed - %s" % guessed)
+    guessed = list(guessed)
     guess = input("insert guess")
     guessed.append(guess)
     guess = guess.lower()
     if guess in answer:
         print("you already guessed this")
+        answer = "".join(answer)
     while guess in word:
         answer = list(answer)
         current_index = word.index(guess)
@@ -49,4 +55,6 @@ while correct > 0 and win is False:
         answer = "".join(answer)
         print("nope")
         correct = correct - 1
+true = "".join(true)
+print("the word was %s" % true)
 print("you could'nt guess it :/")
